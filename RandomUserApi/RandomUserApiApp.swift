ffb03fd2f38baf9d6e999a8b1dcb03116c91d8bb
jacobkosmart <jacobkosmart@gmail.com>
2022-02-18 13:59:56 +0900

    Initial Commit

---
+++ RandomUserApi/RandomUserApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApiApp.swift
+//  RandomUserApi
+//
+//  Created by Jacob Ko on 2022/02/18.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
